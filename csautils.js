ced75684331d94adb62d2b502554bd69e863a19c
george <george@opsrobot.co.uk>
2015-03-09 17:47:51 +0000

    added file parser

--- csautils.js
+++ csautils.js
@@ -1,8 +1,11 @@
 //csautils
 csaUtils = {}
  
-csaUtils.getPropertyPayload = function (global_id, name , value) {
-                return {
+csaUtils.getPropertyPayload = function (global_id, name , value, type) {
+
+type = type.toUpperCase();
+
+return {
     "@type": "",
     "description": "",
     "ext": {
@@ -17,10 +20,28 @@ csaUtils.getPropertyPayload = function (global_id, name , value) {
         "global_id": global_id
     },
     "ownership": null,
-    "property_type": "STRING",
+    "property_type": type,
     "property_value": value
-                }
+     }
 }
- 
+
+csaUtils.getComponentPayload = function ( name , description , consumerVisible) {   
+    var propName = name.toUpperCase().replace(' ','');
+
+    return {
+      "global_id": null,
+      "@type": "",
+      "name": name,
+      "description": description,
+      "icon": "/csa/api/blobstore/other.png?tag=library",
+      "ext": {
+        "csa_critical_system_object": false,
+        "csa_name_key": propName,
+        "csa_consumer_visible": consumerVisible,
+        "csa_pattern": false,
+        "csa_parent": null
+      }
+    }
+}
  
 module.exports = csaUtils;
--- go.js
+++ go.js
@@ -4,12 +4,15 @@ rest = require('restler-q').spread;
 creds = require('./creds');
 csaUtils = require('./csaUtils');
 Q = require('q');
+chalk = require('chalk');
+var glob = require("glob");
+var fs = require('fs');
 
 
 //q = require ('q');
 //var authString = enc(u + ":" + pw);
 var baseUrl = "https://vm01:8444/";
-var data = {
+var credentialData = {
     "passwordCredentials" :
     {
         "username" : creds.u,
@@ -17,7 +20,7 @@ var data = {
     },
     "tenantName" : creds.org
 };
-options = {
+httpOptions = {
     rejectUnauthorized : false,
     username: creds.u,
     password: creds.pw
@@ -27,39 +30,95 @@ IdmCallOptions = {
     username: creds.idmU,
     password: creds.idmPw
 }
-var getTask = function (xAuthToken , payload, path) {
+
+var getTask = function (xAuthToken , payload, path ) {
     return function(){
+
         return new Promise(function(resolve, reject) {
-          rest.postJson(baseUrl + path , payload , options)
-          .spread(function(data){
-            debugger;
-            console.log("worked!")
-            console.log(data)
-            resolve(data);
-        },function(data){
-            debugger;
-            reject(Error(data.code));
+
+            rest.postJson(baseUrl + path , payload , httpOptions)
+            .spread(
+                function(data){
+                    debugger;
+                    console.log("worked!")
+                    console.log(data)
+                    resolve(data);
+
+                },
+                function(data){
+                    debugger;
+                    reject(Error(data.code));
+                }
+            );
+
         });
-      });
     }
 }
 
+function getIdFromName(data , name){
+    var id = ""
+    for (member in data) {
+        if (data[member].name == name) {
+            id = data[member].global_id;
+        }
+    }
+    return id;
+}
 
-console.log("retrieving auth token for " + creds.u + " in organisation " + creds.org);
-rest.postJson(baseUrl + 'idm-service/v2.0/tokens/', data ,IdmCallOptions )
-.spread(function(data){
-    console.log("got token.");
-    var xAuthToken = data.token.id;
-    //var a = ["snRequestId","requestor","owner","server_type","business","comment","requestNumber","count","cluster","domain","Environment","Zone","Subnet","SubnetMask","DefaultGateway","Network","DataCentre","OperatingSystem","VCores","Memory","VDisk1","VDisk2","VDisk3","LifeSpan","BcpTier","BcpScope","BcpPartners","ResourceGroup","ResourceGroupRequester","Tag","DNSDomain","DomainNam","DNSSuffixSearchList","DNSServerList","DefaultDNSSuffix","ApplicationProfile","EnableDhcp","WinsServerList","LdapOU","TimeZone","InputLocale","UserLocale","SiteLocation","Description","Volume1","Volume2","Volume3","Sockets","AquireHostname","AquireHostnam","HostName","Hostnam","AddmDataCentre","PatchDay","PatchSlot","OSPatchedBy","ServerCategoryFunction","Region","PrimaryApplication","BusinessLineMetier","CostCentre","BusinessLine","BuildProfile","Backups","BuildType","DateRequested","Application","Business","Country","SupplementarySecurityRequirements","PowerUserSecurityRequirements","IPAddress","OldHostName"];
-    var properties = ["qwqssww","qwswsqw","ewfwswsw"];
-    var tasks = properties.map(function(prop){
-        console.log("adding property " + prop);
-        return getTask(xAuthToken, csaUtils.getPropertyPayload('2c9030e44bdb3bfe014be0608a7b4209', prop, 'undefined') , "csa/api/property" )
+
+
+fs.readFile('./' + "csaPackage1.json" , 'utf8' , function (err,data) {
+
+    csaPackage = JSON.parse(data);
+
+    debugger;
+
+    //login
+    console.log("retrieving auth token for " + creds.u + " in organisation " + creds.org);
+    rest.postJson(baseUrl + 'idm-service/v2.0/tokens/', credentialData ,IdmCallOptions )
+    .spread(function(data){
+        //logged in
+        console.log("got token.");
+
+        var xAuthToken = data.token.id;
+
+        rest.get(baseUrl +'csa/api/metamodel' , httpOptions)
+        .spread( function(data){
+            var paletteName = "dbCloudManager Abstract Components";
+            console.log(" looking up palette " + paletteName);
+            var paletteId = getIdFromName(data.members , paletteName);
+            return rest.get(baseUrl +'csa/api/metamodel/' + paletteId , httpOptions)
+        }).spread(function(paletteData){
+            var componentName = "DB Internal Server v1.0.0";
+            console.log(" looking up component " + componentName);
+            var componentId = getIdFromName(paletteData.members , componentName);
+            debugger;
+            //componentTask = getTask(xAuthToken, csaUtils.getComponentPayload("New Component" , "this is a new component.") , "csa/api/metamodel/" + "2c9030e44bdb3bfe014be06058354206" );
+
+
+            //var a = ["snRequestId","requestor","owner","server_type","business","comment","requestNumber","count","cluster","domain","Environment","Zone","Subnet","SubnetMask","DefaultGateway","Network","DataCentre","OperatingSystem","VCores","Memory","VDisk1","VDisk2","VDisk3","LifeSpan","BcpTier","BcpScope","BcpPartners","ResourceGroup","ResourceGroupRequester","Tag","DNSDomain","DomainNam","DNSSuffixSearchList","DNSServerList","DefaultDNSSuffix","ApplicationProfile","EnableDhcp","WinsServerList","LdapOU","TimeZone","InputLocale","UserLocale","SiteLocation","Description","Volume1","Volume2","Volume3","Sockets","AquireHostname","AquireHostnam","HostName","Hostnam","AddmDataCentre","PatchDay","PatchSlot","OSPatchedBy","ServerCategoryFunction","Region","PrimaryApplication","BusinessLineMetier","CostCentre","BusinessLine","BuildProfile","Backups","BuildType","DateRequested","Application","Business","Country","SupplementarySecurityRequirements","PowerUserSecurityRequirements","IPAddress","OldHostName"];
+            var properties = ["one","two","three"];
+            var propTasks = properties.map(function(prop){
+                console.log("   adding property " + prop);
+                return getTask(xAuthToken, csaUtils.getPropertyPayload(componentId, prop, 'undefined' , "STRING") , "csa/api/property" )
+            });
+
+
+
+            propTasks.reduce(Q.when, Q('a')).done();
+
+        })
+
+
+    },function(error){
+        console.log("errorall:")
+        debugger;
+        console.log(error.toString())
     });
 
-    tasks.reduce(Q.when, Q('a')).done();
-},function(error){
-    console.log("errorall:")
-    debugger;
-    console.log(error.toString())
+
+
 });
+
+
+
